6d14be63c60425fc5147822cad405180f1058172
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-03-27 09:26:16 +0000

    Add few test

--- script.test.js
+++ script.test.js
@@ -1,3 +1,22 @@
+const googleSearch = require('./script')
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+]
+
 it('Silly test', ()=>{
     expect(true).toBe(true)
+})
+
+it('is searching google', ()=>{
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+})
+
+it('work with undefined or null', ()=>{
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
 })
